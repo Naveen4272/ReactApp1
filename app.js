4d2910382e8c488b741c33878c0f3e194a206c8c
Naveen4272 <naveenkumar4272n@gmail.com>
2025-05-06 04:59:49 +0530

    ep3 React functional components

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 import  React from "react";
 import ReactDOM from "react-dom/client"
 
-const heading =<h1>hello world</h1>
- const root=ReactDOM.createRoot(document.getElementById("root"));
- root.render(heading);
- console.log(heading);
+const title=<h1>Title</h1>
+
+const footer=()=>{
+  return(
+    <h1>footer</h1>
+  )
+  
+}
+const number=1000;
+const Component=()=>{
+  return(
+  <h2>component</h2>
+  )
+}
+
+const HeadingComponent=()=>{
+  return(
+<div>
+  <h2>{number}</h2>
+  {title}
+  {footer()}
+  <Component/>
+    <h1>heading Component</h1>
+    </div>
+  )
+}
+
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>)
  
