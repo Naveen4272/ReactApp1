5e1293484c9918dd4a66d980c1fae2ba099f2a51
Naveen4272 <naveenkumar4272n@gmail.com>
2025-05-06 14:44:50 +0530

    creating the ReactApp

--- app.js
+++ app.js
@@ -1,35 +1,475 @@
 import  React from "react";
 import ReactDOM from "react-dom/client"
 
-const title=<h1>Title</h1>
 
-const footer=()=>{
+const Header = () =>{
   return(
-    <h1>footer</h1>
-  )
-  
-}
-const number=1000;
-const Component=()=>{
-  return(
-  <h2>component</h2>
-  )
-}
-
-const HeadingComponent=()=>{
-  return(
-<div>
-  <h2>{number}</h2>
-  {title}
-  {footer()}
-  <Component/>
-    <h1>heading Component</h1>
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="https://img.freepik.com/premium-vector/restaurant-logo-design-template_79169-56.jpg?w=2000"></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   )
 }
 
 
 
+const RestaurantCard = (props) =>{
+  const{resData}=props;
+  const{name,cuisines,avgRating,costForTwo,cloudinaryImageId}=resData?.info
+  return(
+    <div className="res-card">
+      <img className="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}></img>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(" , ")}</h4>
+      <h4>{avgRating}</h4>
+      <h3>{costForTwo}</h3>
+    </div>
+  )
+}
+
+const resList=[  {
+  "info": {
+    "id": "537056",
+    "name": "Baskin Robbins - Ice Cream Desserts",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/4/24/fb207e93-bcea-467e-9137-7138cea01fd7_537056.JPG",
+    "locality": "Bharathi Park",
+    "areaName": "Saibaba Colony",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+      "Desserts",
+      "Ice Cream"
+    ],
+    "avgRating": 4.5,
+    "veg": true,
+    "parentId": "5588",
+    "avgRatingString": "4.5",
+    "totalRatingsString": "629",
+    "sla": {
+      "deliveryTime": 29,
+      "lastMileTravel": 5,
+      "serviceability": "SERVICEABLE",
+      "slaString": "25-30 mins",
+      "lastMileTravelString": "5.0 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2025-05-07 01:45:00",
+      "opened": true
+    },
+    "badges": {
+      "textExtendedBadges": [
+        {
+          "iconId": "Ratnesh_Badges/test2.png",
+          "shortDescription": "Perfect ice cream delivery",
+          "fontColor": "#7E808C"
+        }
+      ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+
+        },
+        "textBased": {
+
+        },
+        "textExtendedBadges": {
+          "badgeObject": [
+            {
+              "attributes": {
+                "description": "",
+                "fontColor": "#7E808C",
+                "iconId": "Ratnesh_Badges/test2.png",
+                "shortDescription": "Perfect ice cream delivery"
+              }
+            }
+          ]
+        }
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "50% OFF",
+      "subHeader": "UPTO ₹100"
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "lottie": {
+
+        },
+        "video": {
+
+        }
+      }
+    },
+    "reviewsSummary": {
+
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+
+    },
+    "externalRatings": {
+      "aggregatedRating": {
+        "rating": "--"
+      }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+  },
+  "analytics": {
+    "context": "seo-data-1f378df6-1aa5-416e-b436-7a6da7cd0652"
+  },
+  "cta": {
+    "link": "https://www.swiggy.com/city/coimbatore/baskin-robbins-ice-cream-desserts-bharathi-park-saibaba-colony-rest537056",
+    "type": "WEBLINK"
+  }
+},
+{
+  "info": {
+    "id": "440603",
+    "name": "EatFit - All Things Healthy",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/26/e1ca7f45-ef6c-4350-9319-419795a94905_440603.jpg",
+    "locality": "SRP Nagar",
+    "areaName": "Saibaba Colony",
+    "costForTwo": "₹270 for two",
+    "cuisines": [
+      "Chinese",
+      "Healthy Food",
+      "Tandoor",
+      "Pizzas",
+      "North Indian",
+      "Thalis",
+      "Biryani"
+    ],
+    "avgRating": 4.5,
+    "parentId": "76139",
+    "avgRatingString": "4.5",
+    "totalRatingsString": "3.7K+",
+    "sla": {
+      "deliveryTime": 34,
+      "lastMileTravel": 6.6,
+      "serviceability": "SERVICEABLE",
+      "slaString": "30-35 mins",
+      "lastMileTravelString": "6.6 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2025-05-07 01:00:00",
+      "opened": true
+    },
+    "badges": {
+      "textExtendedBadges": [
+        {
+          "iconId": "guiltfree/GF_Logo_android_3x",
+          "shortDescription": "brand",
+          "fontColor": "#7E808C"
+        }
+      ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+
+        },
+        "textBased": {
+
+        },
+        "textExtendedBadges": {
+          "badgeObject": [
+            {
+              "attributes": {
+                "description": "",
+                "fontColor": "#7E808C",
+                "iconId": "guiltfree/GF_Logo_android_3x",
+                "shortDescription": "brand"
+              }
+            }
+          ]
+        }
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "ITEMS",
+      "subHeader": "AT ₹99"
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "lottie": {
+
+        },
+        "video": {
+
+        }
+      }
+    },
+    "reviewsSummary": {
+
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+
+    },
+    "externalRatings": {
+      "aggregatedRating": {
+        "rating": "--"
+      }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+  },
+  "analytics": {
+    "context": "seo-data-1f378df6-1aa5-416e-b436-7a6da7cd0652"
+  },
+  "cta": {
+    "link": "https://www.swiggy.com/city/coimbatore/eatfit-all-things-healthy-srp-nagar-saibaba-colony-rest440603",
+    "type": "WEBLINK"
+  }
+},
+{
+  "info": {
+    "id": "440610",
+    "name": "Great Indian Khichdi by EatFit",
+    "cloudinaryImageId": "6e44fd7f1e5cd9967edfe47c10247671",
+    "locality": "SRP Nagar II",
+    "areaName": "Saibaba Colony",
+    "costForTwo": "₹200 for two",
+    "cuisines": [
+      "Home Food",
+      "Indian",
+      "North Indian",
+      "Healthy Food",
+      "Snacks",
+      "Desserts",
+      "Rajasthani",
+      "South Indian",
+      "Maharashtrian",
+      "Sweets"
+    ],
+    "avgRating": 4.4,
+    "veg": true,
+    "parentId": "319582",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "1.1K+",
+    "sla": {
+      "deliveryTime": 32,
+      "lastMileTravel": 6.1,
+      "serviceability": "SERVICEABLE",
+      "slaString": "30-35 mins",
+      "lastMileTravelString": "6.1 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2025-05-07 01:00:00",
+      "opened": true
+    },
+    "badges": {
+      "imageBadges": [
+        {
+          "imageId": "v1695133679/badges/Pure_Veg111.png",
+          "description": "pureveg"
+        }
+      ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+          "badgeObject": [
+            {
+              "attributes": {
+                "description": "pureveg",
+                "imageId": "v1695133679/badges/Pure_Veg111.png"
+              }
+            }
+          ]
+        },
+        "textBased": {
+
+        },
+        "textExtendedBadges": {
+
+        }
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "ITEMS",
+      "subHeader": "AT ₹99"
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "lottie": {
+
+        },
+        "video": {
+
+        }
+      }
+    },
+    "reviewsSummary": {
+
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+
+    },
+    "externalRatings": {
+      "aggregatedRating": {
+        "rating": "--"
+      }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+  },
+  "analytics": {
+    "context": "seo-data-1f378df6-1aa5-416e-b436-7a6da7cd0652"
+  },
+  "cta": {
+    "link": "https://www.swiggy.com/city/coimbatore/great-indian-khichdi-by-eatfit-srp-nagar-ii-saibaba-colony-rest440610",
+    "type": "WEBLINK"
+  }
+},
+{
+  "info": {
+    "id": "50314",
+    "name": "Pizza Hut",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/4/9/1b8e1bb3-0a64-4665-baf2-c2e16d7c7cf5_50314.jpg",
+    "locality": "DB Road",
+    "areaName": "RS Puram",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+      "Pizzas"
+    ],
+    "avgRating": 4.3,
+    "parentId": "721",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "14K+",
+    "sla": {
+      "deliveryTime": 55,
+      "lastMileTravel": 7,
+      "serviceability": "SERVICEABLE",
+      "slaString": "50-55 mins",
+      "lastMileTravelString": "7.0 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2025-05-07 01:00:00",
+      "opened": true
+    },
+    "badges": {
+      "imageBadges": [
+        {
+          "imageId": "Rxawards/_CATEGORY-Pizza.png",
+          "description": "Delivery!"
+        }
+      ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+          "badgeObject": [
+            {
+              "attributes": {
+                "description": "Delivery!",
+                "imageId": "Rxawards/_CATEGORY-Pizza.png"
+              }
+            }
+          ]
+        },
+        "textBased": {
+
+        },
+        "textExtendedBadges": {
+
+        }
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "50% OFF",
+      "subHeader": "UPTO ₹100"
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "lottie": {
+
+        },
+        "video": {
+
+        }
+      }
+    },
+    "reviewsSummary": {
+
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+
+    },
+    "externalRatings": {
+      "aggregatedRating": {
+        "rating": "--"
+      }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+  },
+  "analytics": {
+    "context": "seo-data-1f378df6-1aa5-416e-b436-7a6da7cd0652"
+  },
+  "cta": {
+    "link": "https://www.swiggy.com/city/coimbatore/pizza-hut-db-road-rs-puram-rest50314",
+    "type": "WEBLINK"
+  }
+},]
+const Body = () =>{
+  return(
+    <div className="body">
+      <div className="search">search</div>
+      <div className="res-container">
+        {
+          resList.map((restaurant)=>
+            <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
+            
+
+          
+        )
+        }
+        
+      
+       
+      </div>
+    </div>
+  )
+}
+
+
+
+const AppLayout = () =>{
+  return(
+    <div className="app">
+      <Header/>
+      <Body/>
+
+    </div>
+  )
+}
+
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>)
+root.render(<AppLayout/>)
  
